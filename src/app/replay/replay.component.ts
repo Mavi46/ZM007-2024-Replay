9a875986c25e02287973f4c5379bf1cc95d3e03a
Raffelke <121097717+Raffelke@users.noreply.github.com>
2025-01-23 13:08:26 +0100

    Update replay.component.ts

--- src/app/replay/replay.component.ts
+++ src/app/replay/replay.component.ts
@@ -395,7 +395,7 @@ requests.post(post_url, json=data)`;
     this.h1TextArray = [
       'Dit was het einde van onze ICT-ervaring. Bedankt voor je deelname!',
       'We houden je op de hoogte als je een prijs gewonnen hebt. Houd je e-mail in de gaten voor meer informatie.',
-      'Neem gerust een kijkje bij de andere projecten die hier te zien zijn en stel je vragen als je meer wilt weten!',
+      'Als je meer over dit project wil weten, stel gerust vragen of bekijk de extra informatie.',
       'Geen zorgen, je gegevens worden niet opgeslagen en blijven volledig privé.',
     ];
     this.h1RotationText = this.h1TextArray[0];
